1369a3e09e0e2746c752224c83f2a5bbb8c537eb
jolajesu <jolajesubimtan@gmail.com>
2023-06-22 06:04:27 -0700

    _putchar - Task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
-* main - Entry point of the program.
-*
-* This function prints "_putchar" followed by a new line.
-*
-* Return: Always 0.
-*/
+ * main - Entry point
+ * Return: Always 0
+ */
 
 int main(void)
+
 {
-	char str[] = "_putchar\n";
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		write(1, &str[i], 1);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
