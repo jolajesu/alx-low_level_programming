620385f3b67d522c87fed351a057a57e611f4791
jolajesu <jolajesubimtan@gmail.com>
2023-06-26 13:38:25 -0700

    I do not fear computers. I fear the lack of them - Task three

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - Prints a string to the standard output followed by a new line.
+ *
+ * @str: The string to be printed.
+ *
+ * Return: none.
+ *
+ */
+
+void _puts(char *str)
+{
+
+	printf("%s\n", str);
+
+}
