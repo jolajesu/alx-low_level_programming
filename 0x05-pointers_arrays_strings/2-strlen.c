45e33cabcebb997543017044a91f2376b705989e
jolajesu <jolajesubimtan@gmail.com>
2023-06-26 13:21:35 -0700

    This report, by its very length, defends itself against the risk of being read - Task two

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ *
+ * @s: Pointer to the input string.
+ *
+ *Return: The length of the string as an integer.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
+
